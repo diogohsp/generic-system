43968ca122e33e7297a42e0394a569eb3da9538f
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-06-06 01:58:57 -0300

    feat: adiciona o método findById para ClientsRepository

--- src/repositories/clients-repository.ts
+++ src/repositories/clients-repository.ts
@@ -3,4 +3,5 @@ import { Client, Prisma } from '@prisma/client'
 export interface ClientsRepository {
   create: (data: Prisma.ClientCreateInput) => Promise<Client>
   findByCpf: (cpf: string) => Promise<Client | null>
+  findById: (id: string) => Promise<Client | null>
 }
