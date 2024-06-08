25d7f18cdf467ee3fb3dbc2e024a2d93b12c1eb4
Vinicius Becaleti <viniciuscbecaleti@gmail.com>
2024-06-08 10:24:29 -0300

    feat: adicion a rota POST  /services

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,12 @@ import { registerMechanic } from './controllers/register-mechanic'
 import { authenticateMechanic } from './controllers/authenticate-mechanic'
 import { createClient } from './controllers/create-client'
 import { deleteClient } from './controllers/delete-client'
+import { createService } from './controllers/create-service'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/mechanics', registerMechanic)
   app.post('/mechanics/sessions', authenticateMechanic)
   app.post('/clients', createClient)
+  app.post('/services', createService)
   app.delete('/clients/:id', deleteClient)
 }
